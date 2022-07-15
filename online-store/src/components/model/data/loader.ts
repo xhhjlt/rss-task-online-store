31e43352376cf65d9dc7d108f8a6f5a44d5de0bd
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-15 15:40:34 +0500

    feat: add file exist and have reqired format check

--- online-store/src/components/model/data/loader.ts
+++ online-store/src/components/model/data/loader.ts
@@ -5,8 +5,20 @@ class Loader implements ILoader<ISpaceshipData> {
     ships: Array<ISpaceshipData>;
 
     constructor(localUrl: string) {
-        const data = require(localUrl);
-        this.ships = data.spacecrafts;
+        let data: { spacecrafts: Array<ISpaceshipData> };
+        try {
+            data = require(localUrl);
+        } catch (e) {
+            console.error("Error: Don't have access to file or file not exist");
+            this.ships = [];
+            return;
+        }
+        if (data?.spacecrafts && Array.isArray(data?.spacecrafts)) {
+            this.ships = data.spacecrafts;
+        } else {
+            console.error("Error: Wrong file format");
+            this.ships = [];
+        }
     }
 
     loadData: () => Array<ISpaceshipData> = () => {
@@ -14,7 +26,36 @@ class Loader implements ILoader<ISpaceshipData> {
     }
 
     private validate: (ships: Array<ISpaceshipData>) => Array<ISpaceshipData> = (ships) => {
-        
+        ships.forEach(ship => {
+            switch (ship.type.trim().toLowerCase()) {
+                case "орбитальный":
+                    ship.type = SpaceflightTypes.orbital;
+                    break;
+                case "межпланетный":
+                    ship.type = SpaceflightTypes.interplanetary;
+                    break;
+                case "межзвездный":
+                    ship.type = SpaceflightTypes.interstellar;
+                    break;
+                default:
+                    ship.type = SpaceflightTypes.unknown;
+                    break;
+            }
+            switch (ship.landing.trim().toLowerCase()) {
+                case "мягкая":
+                    ship.landing = LandingType.soft;
+                    break;
+                case "жесткая":
+                    ship.landing = LandingType.crash;
+                    break;
+                case "нет":
+                    ship.landing = LandingType.non;
+                    break;
+                default:
+                    ship.landing = LandingType.unknown;
+                    break;
+            }
+        });
         return ships;    
     }
     
