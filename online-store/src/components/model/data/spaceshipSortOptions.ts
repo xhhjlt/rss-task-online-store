f3d1dfe08e1e08fa04b518538bdfeda9eb27a868
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-15 18:45:07 +0500

    fix: fix year sort option

--- online-store/src/components/model/data/spaceshipSortOptions.ts
+++ online-store/src/components/model/data/spaceshipSortOptions.ts
@@ -1,8 +1,8 @@
 enum spaceshipSortOptions {
     nameForward = "По названию, от A до Я",
     nameBackward = "По названию, от Я до A",
-    capacityForward = "По году запуска, по возрастанию",
-    capacityBackward = "По году запуска, по убыванию",
+    yearForward = "По году запуска, по возрастанию",
+    yearBackward = "По году запуска, по убыванию",
     crewForward = "По экипажу, по возрастанию",
     crewBackward = "По экипажу, по убыванию",
     none = "Сортировка"
