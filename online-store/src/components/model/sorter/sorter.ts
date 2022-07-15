331a2f2bca3353b0c08aaa1bd53c18b36f85f2bf
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-15 21:44:56 +0500

    refactor: add private modificator fore some sorter methods

--- online-store/src/components/model/sorter/sorter.ts
+++ online-store/src/components/model/sorter/sorter.ts
@@ -36,31 +36,31 @@ class Sorter implements ISorter<spaceshipSortOptions, ISpaceshipData> {
         return ships;
     }
 
-    byName(a: ISpaceshipData, b: ISpaceshipData) {
+    private byName(a: ISpaceshipData, b: ISpaceshipData) {
         if (a.name > b.name) return 1;
         if (a.name < b.name) return -1;
         return 0;
     }
 
-    byNameBack(a: ISpaceshipData, b: ISpaceshipData) {
+    private byNameBack(a: ISpaceshipData, b: ISpaceshipData) {
         if (a.name > b.name) return -1;
         if (a.name < b.name) return 1;
         return 0;
     }
 
-    byYear(a: ISpaceshipData, b: ISpaceshipData) {
+    private byYear(a: ISpaceshipData, b: ISpaceshipData) {
         return a.launchYear - b.launchYear;
     }
 
-    byYearBack(a: ISpaceshipData, b: ISpaceshipData) {
+    private byYearBack(a: ISpaceshipData, b: ISpaceshipData) {
         return b.launchYear - a.launchYear;
     }
 
-    byCrew(a: ISpaceshipData, b: ISpaceshipData) {
+    private byCrew(a: ISpaceshipData, b: ISpaceshipData) {
         return a.crew - b.crew;
     }
 
-    byCrewBack(a: ISpaceshipData, b: ISpaceshipData) {
+    private byCrewBack(a: ISpaceshipData, b: ISpaceshipData) {
         return b.crew - a.crew;
     }
 }
