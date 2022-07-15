03dc803517b0a8479f1d979f10f4d14d22108758
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-15 20:41:13 +0500

    feat: add sort with options for sorter test

--- online-store/__test__/sorter.test.ts
+++ online-store/__test__/sorter.test.ts
@@ -1,16 +1,52 @@
+import ISpaceshipData from "../src/components/model/data/ISpaceshipData";
 import spaceshipSortOptions from "../src/components/model/data/spaceshipSortOptions";
 import Sorter from "../src/components/model/sorter/sorter"
 
 let ships = require("./mocks/sorter/correct.json").spacecrafts;
+const sorter = new Sorter();
 
 test('return empty array if nothing to sort', () => {
-    const sorter = new Sorter();
     expect(sorter.useOn([])).toEqual([]);
-})
+});
 
 test('return same array if sort option is none', () => {
-    const sorter = new Sorter();
     sorter.currentSort = spaceshipSortOptions.none;
     expect(sorter.useOn(ships)).toEqual(ships);
+});
+
+test('return sorted by name array if sort option is nameForward', () => {
+    sorter.currentSort = spaceshipSortOptions.nameForward;
+    let sorted = require("./mocks/sorter/sortedByName.json").spacecrafts;
+    expect(sorter.useOn(ships)).toEqual(sorted);
+});
+
+test('return backward sorted by name array if sort option is nameBackward', () => {
+    sorter.currentSort = spaceshipSortOptions.nameBackward;
+    let sorted = require("./mocks/sorter/sortedByNameBack.json").spacecrafts;
+    expect(sorter.useOn(ships)).toEqual(sorted);
+})
+
+test('return sorted by name array if sort option is yearForward', () => {
+    sorter.currentSort = spaceshipSortOptions.yearForward;
+    let sorted = require("./mocks/sorter/sortedByYear.json").spacecrafts;
+    expect(sorter.useOn(ships)).toEqual(sorted);
+})
+
+test('return sorted by name array if sort option is yearBackward', () => {
+    sorter.currentSort = spaceshipSortOptions.yearBackward;
+    let sorted = require("./mocks/sorter/sortedByYearBack.json").spacecrafts;
+    expect(sorter.useOn(ships)).toEqual(sorted);
+})
+
+test('return sorted by name array if sort option is crewForward', () => {
+    sorter.currentSort = spaceshipSortOptions.crewForward;
+    let sorted = require("./mocks/sorter/sortedByCrew.json").spacecrafts;
+    expect(sorter.useOn(ships)).toEqual(sorted);
+})
+
+test('return sorted by name array if sort option is crewBackward', () => {
+    sorter.currentSort = spaceshipSortOptions.crewBackward;
+    let sorted = require("./mocks/sorter/sortedByCrewBack.json").spacecrafts;
+    expect(sorter.useOn(ships)).toEqual(sorted);
 })
 
