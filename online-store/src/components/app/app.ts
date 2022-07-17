6ea155c22be7ba3881f8e885ffbff74096f9c2d6
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-17 13:41:03 +0500

    fix: fix app class

--- online-store/src/components/app/app.ts
+++ online-store/src/components/app/app.ts
@@ -1,14 +1,15 @@
 import Controller from "../controller/controller";
+import Model from "../model/model";
 import View from "../view/view";
 import IApp from "./IApp";
 
 class App implements IApp {
     controller: Controller;
-    view: View;
+
     constructor() {
         this.controller = new Controller;
-        this.view = new View;
     }
+    
     start() {
         console.log("Start App");
         
