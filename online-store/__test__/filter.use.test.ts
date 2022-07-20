0b3abe8ea747f8ba328392822be6ca7d475b2adc
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-20 12:06:14 +0500

    feat: add search testing to filter.use test

--- online-store/__test__/filter.use.test.ts
+++ online-store/__test__/filter.use.test.ts
@@ -45,6 +45,37 @@ test('filter.use() should return array with elements matches crew', () => {
     }])
 })
 
+test('filter.use() should return array with elements matches "search" substring in name', () => {
+    const search = "ar";
+    expect(filter.use(ships, {search})).toEqual([
+        {
+            "id": 8,
+            "name": "NEAR Shoemaker",
+            "image": "Near_Shoemaker.jpeg",
+            "description": "Автоматическая межпланетная станция NASA, отправленная к астероиду Эрос. Стал первым искусственным спутником астероида, и первым искусственным объектом, совершившим мягкую посадку на астероид.",
+            "quote": "Ранее «NEAR spacecraft», получил сегодняшнее название в честь американского геолога Юджина Шумейкераю",
+            "manufacturer": "США",
+            "launchYear": 1996,
+            "crew": 0,
+            "type": "межпланетный",
+            "landing": "мягкая",
+            "inFlight": false
+        },
+        {
+            "id": 9,
+            "name": "DART",
+            "image": "DART.png",
+            "description": "Double Asteroid Redirection Test — испытания перенаправления двойного астероида) — первый в истории проект по изменению траектории астероидов и их перенаправлению.",
+            "quote": "DART намеренно врежется во второй астероид системы Дидим на скорости примерно 6,6 км/с",
+            "manufacturer": "США",
+            "launchYear": 2021,
+            "crew": 0,
+            "type": "межпланетный",
+            "landing": "жесткая",
+            "inFlight": true
+        }]);
+})
+
 test('filter.use() should return array with elements matches landing', () => {
     const landing = new Set<LandingType>([LandingType.crash, LandingType.none]);
     expect(filter.use(ships, {landing})).toEqual([
