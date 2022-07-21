4b5ce66aff7d3d2b011128ebfeed3fe8cfd007e1
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-21 19:09:13 +0500

    feat: add storage.addToCart() test

---
+++ online-store/__test__/storage.addToCart.test.ts
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import DataStorage from "../src/components/model/storage/storage";
+
+const storage = new DataStorage();
+
+test(`storage.addToCart(str) should add str to storage.cart set`, () => {
+    storage.addToCart("anystring");
+    expect(storage.cart.has("anystring")).toEqual(true);
+})
+
+test(`storage.addToCart(str) should add "str" to localStorage`, () => {
+    storage.addToCart("42");
+    expect(localStorage.getItem("42")).toBeTruthy();
+})
