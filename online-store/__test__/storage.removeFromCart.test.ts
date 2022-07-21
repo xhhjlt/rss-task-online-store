aa12e395527f45ed78f56f5de55a994c66e97d55
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-21 19:54:28 +0500

    feat: add storage.removeFromCart() test

---
+++ online-store/__test__/storage.removeFromCart.test.ts
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import DataStorage from "../src/components/model/storage/storage";
+
+const storage = new DataStorage();
+
+test(`storage.removeFromCart(str) should remove str from storage.cart set`, () => {
+    storage.addToCart("anystring");
+    storage.removeFromCart("anystring");
+    expect(storage.cart.has("anystring")).toEqual(false);
+})
+
+test(`storage.removeFromCart(str) should remove "str" from localStorage`, () => {
+    storage.addToCart("42");
+    storage.removeFromCart("42");
+    expect(localStorage.getItem("42")).toBeFalsy();
+})
