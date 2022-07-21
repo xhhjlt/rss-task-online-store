4c3c004b66ca7a02da4fdb2ff8404f3fa49f73e6
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-21 18:38:53 +0500

    feat: add test for product.view()

---
+++ online-store/__test__/products.view.test.ts
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+import ISpaceshipData from "../src/components/model/data/ISpaceshipData";
+import Products from "../src/components/view/products/products";
+
+const cartCounter = document.createElement("span");
+cartCounter.classList.add("cart-count");
+document.body.append(cartCounter);
+const conteiner = document.createElement("section");
+conteiner.classList.add("products-conteiner");
+document.body.append(conteiner);
+const products = new Products;
+const ships = require("./stabs/correct.json").spacecrafts as Array<ISpaceshipData>;
+
+test("products.view should write how much products in cart within cart-count element", () => {
+    const cart = new Set<string>(["1", "2", "5", "9"]);
+    products.view([], cart);
+    expect(cartCounter.innerHTML).toEqual("4");
+})
+
+test("products.view should add cards to products-conteiner", () => {
+    const cart = new Set<string>();
+    products.view(ships.slice(0,2), cart);
+    expect(conteiner.querySelectorAll(".card")).toHaveLength(2);
+})
+
+test("products.view should add class in-cart for each card, which product in cart", () => {
+    const cart = new Set<string>(["1", "2", "5", "9"]);
+    products.view(ships, cart);
+    expect(conteiner.querySelectorAll(".card.in-cart")).toHaveLength(4);
+})
--- online-store/jest.config.js
+++ online-store/jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__test__/stabs/fileMock.ts",
+    "\\.(css|less)$": "<rootDir>/__test__/stabs/fileMock.ts"
+  },
 };
