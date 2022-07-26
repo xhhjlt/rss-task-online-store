ae8d73f8e869f10b6e5db2e7d868939d9417b936
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-07-26 19:31:37 +0500

    refactor: adjust code structure in filters.ts

--- online-store/src/components/view/filters/filters.ts
+++ online-store/src/components/view/filters/filters.ts
@@ -1,6 +1,5 @@
 import { Country, LandingType, SpaceflightTypes } from '../../model/data/ISpaceshipData';
 import ISpaceshipFilters from '../../model/data/ISpaceshipFilters';
-import spaceshipSortOptions from '../../model/data/spaceshipSortOptions';
 import IFiltersView from './IFiltersView';
 import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
@@ -29,89 +28,58 @@ class Filters implements IFiltersView {
 
     constructor() {
         this.conteiner = document.querySelector('.filters-conteiner') as HTMLElement;
+
         this.ussr = document.querySelector('#ussr') as HTMLInputElement;
         this.usa = document.querySelector('#usa') as HTMLInputElement;
         this.china = document.querySelector('#china') as HTMLInputElement;
         this.eu = document.querySelector('#eu') as HTMLInputElement;
+
         this.orbital = document.querySelector('#orbital') as HTMLInputElement;
         this.interplanetary = document.querySelector('#interplanetary') as HTMLInputElement;
         this.interstellar = document.querySelector('#interstellar') as HTMLInputElement;
+
         this.soft = document.querySelector('#soft') as HTMLInputElement;
         this.crash = document.querySelector('#crash') as HTMLInputElement;
         this.none = document.querySelector('#none') as HTMLInputElement;
-        const year: HTMLElement = document.getElementById('year') as HTMLElement;
-        const crew: HTMLElement = document.getElementById('crew') as HTMLElement;
+
         this.crewLabel = document.querySelector('.crew-label') as HTMLElement;
         this.yearLabel = document.querySelector('.year-label') as HTMLElement;
-        this.yearSlider = noUiSlider.create(year, {
-            start: [1950, 2022],
-            connect: true,
-            range: {
-                'min': 1950,
-                'max': 2022
-            },
-            step: 1,
-        });
-        this.crewSlider = noUiSlider.create(crew, {
-            start: [0, 3],
-            connect: true,
-            range: {
-                'min': 0,
-                'max': 3
-            },
-            step: 1,
-        });
+        this.yearSlider = this.createYearSlider();
+        this.crewSlider = this.createCrewSlider();
+
         this.flight = document.querySelector('#flight') as HTMLInputElement;
+
         this.search = document.querySelector('#search') as HTMLInputElement;
+
         this.sort = document.querySelector('#sort') as HTMLInputElement;
+        
         this.resetFilters = document.querySelector('.reset-filters') as HTMLElement;
         this.resetAll = document.querySelector('.reset-all') as HTMLElement; 
     }
 
     getFilters(): ISpaceshipFilters {
         const result: ISpaceshipFilters = {};
-        result.manufacturer = [];
-        if (this.ussr.checked) result.manufacturer.push(Country.ussr);
-        if (this.usa.checked) result.manufacturer.push(Country.usa);
-        if (this.china.checked) result.manufacturer.push(Country.china);
-        if (this.eu.checked) result.manufacturer.push(Country.eu);
-        result.type = [];
-        if (this.orbital.checked) result.type.push(SpaceflightTypes.orbital);
-        if (this.interplanetary.checked) result.type.push(SpaceflightTypes.interplanetary);
-        if (this.interstellar.checked) result.type.push(SpaceflightTypes.interstellar);
-        result.landing = [];
-        if (this.soft.checked) result.landing.push(LandingType.soft);
-        if (this.crash.checked) result.landing.push(LandingType.crash);
-        if (this.none.checked) result.landing.push(LandingType.none);
-        result.inFlight = this.flight?.checked;
-        result.search = this.search.value;
         const crewRange: Array<number> = this.crewSlider.get(true) as Array<number>;
-        [result.crewMin, result.crewMax] = crewRange;
         const yearRange: Array<number> = this.yearSlider.get(true) as Array<number>;
+
+        result.manufacturer = [Country.ussr, Country.usa, Country.china, Country.eu]
+            .filter((_item, index) => 
+                [this.ussr.checked, this.usa.checked, this.china.checked, this.eu.checked][index]);
+
+        result.type = [SpaceflightTypes.orbital, SpaceflightTypes.interplanetary, SpaceflightTypes.interstellar]
+            .filter((_item, index) => [this.orbital.checked, this.interplanetary.checked, this.interstellar.checked][index]);
+        
+        result.landing = [LandingType.soft, LandingType.crash, LandingType.none]
+            .filter((_item, index) => [this.soft.checked, this.crash.checked, this.none.checked][index]);
+
+        result.inFlight = this.flight?.checked;
+
+        result.search = this.search.value;
+        
+        [result.crewMin, result.crewMax] = crewRange;
         [result.launchYearMin, result.launchYearMax] = yearRange;
-        switch (this.sort.value) {
-        case '1':
-            result.sort = spaceshipSortOptions.nameForward;
-            break;
-        case '2':
-            result.sort = spaceshipSortOptions.nameBackward;
-            break;
-        case '3':
-            result.sort = spaceshipSortOptions.yearForward;
-            break;
-        case '4':
-            result.sort = spaceshipSortOptions.yearBackward;
-            break;
-        case '5':
-            result.sort = spaceshipSortOptions.crewForward;
-            break;
-        case '6':
-            result.sort = spaceshipSortOptions.crewBackward;
-            break;
-        default:
-            result.sort = spaceshipSortOptions.nameForward;
-            break;
-        }
+
+        result.sort = +this.sort.value;
 
         return result;
     }
@@ -121,17 +89,52 @@ class Filters implements IFiltersView {
         this.usa.checked = Boolean(filters.manufacturer?.includes(Country.usa));
         this.china.checked = Boolean(filters.manufacturer?.includes(Country.china));
         this.eu.checked = Boolean(filters.manufacturer?.includes(Country.eu));
+
         this.orbital.checked = Boolean(filters.type?.includes(SpaceflightTypes.orbital));
         this.interplanetary.checked = Boolean(filters.type?.includes(SpaceflightTypes.interplanetary));
         this.interstellar.checked = Boolean(filters.type?.includes(SpaceflightTypes.interstellar));
+
         this.soft.checked = Boolean(filters.landing?.includes(LandingType.soft));
         this.crash.checked = Boolean(filters.landing?.includes(LandingType.crash));
         this.none.checked = Boolean(filters.landing?.includes(LandingType.none));
+
         this.flight.checked = Boolean(filters.inFlight);
+
         this.sort.value = filters.sort ? filters.sort.toString() : '1';
+
         this.search.value = filters.search || '';
+
         this.crewSlider.set([(filters.crewMin ? filters.crewMin : 0) , (filters.crewMax ? filters.crewMax : 3)]);
         this.yearSlider.set([(filters.launchYearMin ? filters.launchYearMin : 1950) , (filters.launchYearMax ? filters.launchYearMax : 2022)]);
+    }
+
+    private createCrewSlider() {
+        const crew: HTMLElement = document.getElementById('crew') as HTMLElement;
+
+        return noUiSlider.create(crew, {
+            start: [0, 3],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': 3
+            },
+            step: 1,
+        });
+    }
+
+    private createYearSlider() {
+        const year: HTMLElement = document.getElementById('year') as HTMLElement;
+
+        return noUiSlider.create(year, {
+            start: [1950, 2022],
+            connect: true,
+            range: {
+                'min': 1950,
+                'max': 2022
+            },
+            step: 1,
+        });
+        
     }
 }
 
